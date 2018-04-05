9ace6683bd3282498497b40a93a80197ea068788
Ricardo Mayer <ricardomayerb@gmail.com>
2018-04-05 02:46:17 -0300

    fit arimas usimg info in optoma

--- R/argentina_sarimax_rm.R
+++ R/argentina_sarimax_rm.R
@@ -9,7 +9,68 @@ library(lubridate)
 data_path <- "./data/excel_data/Argentina.xlsx"
 
 arg_m <- read_excel(data_path, sheet = "monthly")
+country_m <- arg_m 
+start_year <- min(country_m$year)
+start_month <- 1
+this_start  <- c(start_year, start_month)
+arima_names <- names(country_m)[5:ncol(country_m)]
+data_m_ts <- tk_ts(country_m, frequency = 12, 
+                   start = this_start)
+data_m_ts <- data_m_ts[ , arima_names]
 
+country_dmetra <- read_excel(data_path, sheet = "optimal")
+country_dmetra <- country_dmetra %>% 
+  separate(data = ., col = 1, into = c("freq_type", "id"))
+
+country_rgdp_dmetra <- country_dmetra %>% 
+  filter(id == "rgdp") %>% select(- freq_type)
+  
+country_monthly_dmetra <- country_dmetra %>% 
+  filter(id != "rgdp") %>% select(- freq_type)
+
+
+rgdp_order <- c(country_rgdp_dmetra$P, country_rgdp_dmetra$D,
+                country_rgdp_dmetra$Q)
+
+rgdp_seasonal <- c(country_rgdp_dmetra$BP, country_rgdp_dmetra$BD, 
+                   country_rgdp_dmetra$BQ)
+
+rgdp_inc_mean <- ifelse(country_rgdp_dmetra$Mean == 1, TRUE, FALSE)
+
+# rgdp_arima_dmetra <- Arima(y = log_rgdp, order = rgdp_order,
+#                            seasonal = rgdp_seasonal, 
+#                            include.mean = rgdp_inc_mean)
+
+tic()
+for (i in seq_along(country_monthly_dmetra$id)) {
+  
+  print(i)
+  
+  this_order <- c(country_monthly_dmetra$P[i], country_monthly_dmetra$D[i],
+                  country_monthly_dmetra$Q[i])
+  
+  this_seasonal <- c(country_monthly_dmetra$BP[i], country_monthly_dmetra$BD[i], 
+                     country_monthly_dmetra$BQ[i])
+  
+  inc_mean <- ifelse(country_monthly_dmetra$Mean[i] == 1, TRUE, FALSE)
+  
+  # this_arima_lev <- Arima(data_m_ts[,i], order = this_order,
+  #                         seasonal = this_seasonal, include.mean = inc_mean)
+  # 
+  # this_arima_log <- Arima(log(data_m_ts[,i]), order = this_order,
+  #                         seasonal = this_seasonal, include.mean = inc_mean)
+
+  this_arima_lam <- Arima(data_m_ts[,i], order = this_order,
+                          seasonal = this_seasonal, include.mean = inc_mean,
+                          lambda = 0, biasadj = TRUE)
+
+}
+toc()
+
+# p q P Q freq d D  
+foo <- this_arima_lam[["arma"]]
+names(foo) <- c("p", "q", "P", "Q", "freq", "d", "D")
+foo 
 
 # playing with dates, to educate ourselves
 final_date_dec <- 2019 + (11/12) 
@@ -23,19 +84,6 @@ month(date_decimal(final_date_dec))
 zoo::as.yearmon(final_date_dec)
 
 
-country_m <- arg_m 
-
-start_year <- min(country_m$year)
-start_month <- 1
-
-this_start  <- c(start_year, start_month)
-
-arima_names <- names(country_m)[5:ncol(country_m)]
-
-data_m_ts <- tk_ts(country_m, frequency = 12, 
-                   start = this_start)
-
-data_m_ts <- data_m_ts[ , arima_names]
 
 arima_list_m <- list()
 
